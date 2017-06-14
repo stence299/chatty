dbc4942d0da7bfd0ac67b539a35df568bf17218b
stence299 <stence299@gmail.com>
2017-06-13 22:37:47 -0500

    doonneee

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var app = express()
+
+app.use(express.static("assets"))
+app.use(bodyParser.json())
+
+var messages = [{message: "Type", time: "Today"}];
+
+app.get("/messages", function(req, res, next) {
+    res.send(messages)
+})
+app.post("/messages", function (req, res, next){
+    console.log(req.body.message);
+    messages.push({ message: req.body.message, time: new Date() })
+    res.status(200).json({messages: messages});
+})
+
+app.listen(3000)
+console.log("listening")
 
